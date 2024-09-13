64dfc41375f48b60beb75a9d1ce8ce37bb527cd6
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2024-09-14 00:07:55 +0300

    Created a Modular archoitecture
    Created a modular architecture for the hand tracking system for scalability

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -14,12 +14,22 @@ cTime = 0
 
 while True:
     success, img = cap.read()
+    # Flip the image horizontally (mirror effect)
+    img = cv2.flip(img, 1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 4:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) 
 
     cTime = time.time()
@@ -27,8 +37,6 @@ while True:
     pTime = cTime
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
